9feea89286872560f622133d13c950181cc4621c
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-11-18 19:55:36 +0300

    les09

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -44,6 +44,16 @@ public class Main {
         System.out.println(MyMath.area(5));
         System.out.println(MyMath.sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
+        String s = "1000";
+        int n = Integer.parseInt(s);
+        System.out.println(n);
 
+        String sent = "This is John. He is 27 years old.";
+        String name = sent.substring(8, 12);
+        System.out.println(name);
+        int age = Integer.parseInt(sent.substring(20, 22));
+        System.out.println(age);
+        Person person = new Person(name, age);
+        System.out.println(person.getName() + " " + person.getAge());
     }
 }
